3cf42e4535a3320866142e1c14f926be8e8c75fa
Caramiozz <56002327+Caramiozz@users.noreply.github.com>
2022-01-12 15:25:27 +0300

    Update register.php

--- register.php
+++ register.php
@@ -17,7 +17,7 @@ session_start();
     <script type="text/javascript" src="javascriptfile.js"></script>
     
    
-  
+     <title>Sign Up</title>
     
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
