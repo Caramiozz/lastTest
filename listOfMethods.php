d473ea3f639f905c8228b21a60cad26fb48545bc
Caramiozz <56002327+Caramiozz@users.noreply.github.com>
2022-01-12 14:31:28 +0300

    Add files via upload

---
+++ listOfMethods.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+    <head>
+        <!--fontawesome icons-->
+        
+        
+        
+        <!-- include the javascript file -->
+        <script type="text/javascript" src="javascriptfile.js"></script>
+       
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        
+        <title>Document</title>
+
+        <!-- include the CSS file -->
+        <link rel="stylesheet" href="main.css">
+        
+        
+        
+        <!-- Confirm resubmission is avoided this way for now... -->
+        <script>
+            if ( window.history.replaceState )
+            {
+                window.history.replaceState( null, null, window.location.href );
+            }
+        </script>
+        
+         
+    </head>
+
+<div id="wrapper">
+        
+        
+        
+    
+<body>
+
+
+
+</body>
+
+</div>
+    
+</html>
